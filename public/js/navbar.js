fe580e36dc0df69c3025e80d4bfcd28750141569
Divyesh Vartha <divyeshvartha@gmail.com>
2022-11-09 21:26:19 +0530

    preserve navbar title across pages

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,10 +1,21 @@
+const list = ["तांगो", "તાંગો", "単語", "tango", "たんご", "タンゴ"];
+let i;
+if (!isNaN(parseInt(localStorage.getItem('titleIndex')))) {
+    i = parseInt(localStorage.getItem('titleIndex'));
+    console.log(localStorage.getItem('titleIndex'));
+    document.querySelector("h1").innerText = list[i];
+}
+else {
+    i = 2;
+}
 
-const changeH1 = (i, list) => () => {
-    document.querySelector("h1").innerText = list[i % list.length];
-    i++;
-    if (i === list.length)
-        i = 0;
+console.log(i);
+
+const changeH1 = () => () => {
+    ++i;
+    if (i >= list.length) { i = 0; }
+    document.querySelector("h1").innerText = list[i];
+    localStorage.setItem('titleIndex', i);
 };
 
-
-setInterval(changeH1(0, ["तांगो", "તાંગો", "単語", "tango", "たんご", "タンゴ"]), 7000);
+setInterval(changeH1(), 7000);
