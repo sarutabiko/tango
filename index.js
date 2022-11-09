63b868c2a82b5cbb88db4d302f7bc11fa770009a
Divyesh Vartha <divyeshvartha@gmail.com>
2022-11-09 21:47:18 +0530

    Added user routing, user registration via passport modules, basic error handling, session and flash messages

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const methodOverride = require("method-override");
 
 const ejsMate = require("ejs-mate");
 
-const app = express();
-app.set("views", path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, 'public')));
-
 const mongoose = require('mongoose');
 const { Word } = require('./models/wordSchema');
+
+const session = require("express-session");
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const userRoutes = require('./routes/users');
+const flash = require('connect-flash');
+
+const ExpressError = require('./utils/ExpressError');
 
 // mongo connection
 main()
     .then(() => {
-        console.log("Connection Open!!!");
+        console.log("Database Connection Successful!!!");
     })
     .catch(
         err => { console.log("Ooops error!!!"); console.log(err); });
@@ -29,33 +30,69 @@ async function main() {
 
 //express route handling
 
+const app = express();
+
+app.set("views", path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(flash());
+
+app.use((req, res, next) => {
+    console.log(req.session);
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 app.listen(3333, '0.0.0.0', () => {
     console.log("App is listening on 3333 port.");
 })
 
+app.use('/', userRoutes);
 
 app.get('/', (req, res) => {
     res.render('index', { title: ": one, simple" });
 })
 
 app.get('/search', (req, res) => {
-    res.render('search', { title: "単語 | tango | たんご" });
+    res.render('search', { title: "Search" });
 })
 
 app.get('/add', (req, res) => {
     res.render('add', { title: 'Add word' });
 })
 
-app.get('/register', (req, res) => {
-    res.render('register', { title: 'Register' });
-})
-
-app.post('/authenticate', (req, res) => {
-    res.send(req.body);
+app.all('*', (req, res, next) => {
+    next(new ExpressError(404, 'Page Not Found'))
 })
 
 app.use((err, req, res, next) => {
     console.log("Something went wrong!");
     console.log(err);
-    next(err);
+    if (!(err.statusCode))
+        err.statusCode = 500;
+    if (!err.message)
+        err.message = "Oh no. Something went wrong."
+    res.status(err.statusCode).render('error', { err, title: "Error" });
 })
