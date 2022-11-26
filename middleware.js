7aea3ef78f90d3209ab0d703ea854e60f10938c2
Divyesh Vartha <divyeshvartha@gmail.com>
2022-11-26 14:16:12 +0530

    Change 'isLoggedIn' to block and redirect, and give old functionality to 'promptLogin'

--- middleware.js
+++ middleware.js
@@ -1,9 +1,17 @@
 const { Wordlist } = require("./models/wordSchema");
 
-module.exports.isLoggedIn = (req, res, next) => {
+module.exports.promptLogin = (req, res, next) => {
     if (!req.isAuthenticated()) {
         res.locals.message = 'You must be logged in to do that.';
         return next();
+    }
+    next();
+}
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash('error', "You must be logged in.");
+        return res.redirect('/auth');
     }
     next();
 }
