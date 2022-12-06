3ffd737e5a2b1f7119118ac1fc38bd848e042b7e
Divyesh Vartha <divyeshvartha@gmail.com>
2022-12-06 11:44:14 +0530

    Add a post hook and a list "length" virtual
    The post "save" hook will update the word/list count in a User object.

--- models/wordSchema.js
+++ models/wordSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { User } = require('./user');
 
 const word = new mongoose.Schema({
     Kanji: {
@@ -40,8 +41,25 @@ const wordlist = new mongoose.Schema({
     }
 })
 
+wordlist.virtual('length').get(function () {
+    return this.words.length;
+})
+
+wordlist.post('save', async function (doc) {
+    console.log("ownder id is: ", doc.owner._id);
+
+    const getOwner = await User.findById(doc.owner._id).populate('lists');
+    let wordN = 0;
+    for (const list of getOwner.lists)
+        wordN += list.length;
+    getOwner.listCount = getOwner.lists.length;
+    getOwner.wordCount = wordN;
+    getOwner.save();
+})
+
 const Word = mongoose.model('Word', word);
 const Wordlist = mongoose.model('Wordlist', wordlist);
 module.exports = { Word, Wordlist };
+
 
 
